b3f5dcc0316d314ce1548e270902311849b5b99e
Molly Montgomery <Molly@mollys-air-2.lan>
2022-02-25 19:53:22 -0600

    added idb.js to files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,12 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
+    "./",
     "./index.html",
     "./manifest.json",
     "./css/style.css",
     "./js/index.js",
+    "./js/idb.js",
     "./icons/icon-72x72.png",
     "./icons/icon-96x96.png",
     "./icons/icon-128x128.png",
