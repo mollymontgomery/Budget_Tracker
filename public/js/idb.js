2d4f51c9fc6a6bd97d6c6874b5689d5e8ef555e8
Molly Montgomery <Molly@mollys-air-2.lan>
2022-02-25 19:41:10 -0600

    added request on success and error and functions for upload and saving

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,3 +6,33 @@ request.onupgradeneeded = function (event) {
     const db = event.target.result;
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const budgetObjectStore = transaction.ObjectStore('new_transaction');
+
+    budgetObjectStore.add(record);
+};
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    
+    const budgetObjectStore = transaction.onjectStore('new_transaction');
+
+    const getAll.onsuccess = function() {
+
+    }
+}
