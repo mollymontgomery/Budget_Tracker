b429b150f14635989c4ac426bc934f864be9f182
Molly Montgomery <Molly@mollys-air-2.lan>
2022-02-25 19:43:25 -0600

    added if/then for getAll results and event listener

--- public/js/idb.js
+++ public/js/idb.js
@@ -15,7 +15,7 @@ request.onsuccess = function (event) {
     }
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
     console.log(event.target.errorCode);
 };
 
@@ -29,10 +29,37 @@ function saveRecord(record) {
 
 function uploadTransaction() {
     const transaction = db.transaction(['new_transaction'], 'readwrite');
-    
+
     const budgetObjectStore = transaction.onjectStore('new_transaction');
 
-    const getAll.onsuccess = function() {
+    const getAll = budgetObjectStore.getAll();
 
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+                    budgetObjectStore.clear();
+
+                    alert('All saved transactions has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
     }
 }
+
+window.addEventListener('online', uploadTransaction);
